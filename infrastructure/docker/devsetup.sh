560b97a8a75ce5deb82e00216b1dec62c0f8fb0a
solmon <solmon@gmail.com>
2024-09-04 18:52:07 +0530

    changes to the model for services

--- infrastructure/docker/devsetup.sh
+++ infrastructure/docker/devsetup.sh
@@ -5,3 +5,4 @@ docker run --name mongodb -p 27017:27017 -d mongodb/mongodb-community-server:lat
 # pnpm --filter=crm exec npx @snaplet/seed init prisma/seed
 # "seed": "ts-node prisma/seed/seed.ts"
 # postgresql://fielduser:Yarn@2021@localhost/crm_db?connect_timeout=10&application_name=cc
+# mongodb://localhost:27017/services?authSource=admin&directConnection=true
